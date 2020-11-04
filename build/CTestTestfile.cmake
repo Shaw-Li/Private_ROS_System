508a148ca4f8955d634b5bbb5cd5a73cf5d33119
ShawLi <1048796174@qq.com>
2020-11-04 17:01:57 +0800

    create workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/myho/Private_repositories/Private_ROS_System/src
+# Build directory: /home/myho/Private_repositories/Private_ROS_System/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/myho/Private_repositories/Private_ROS_System/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/myho/ROS/ipc/devel;/opt/ros/kinetic'.split(';') if '/home/myho/ROS/ipc/devel;/opt/ros/kinetic' != '' else []
